c4d48861d4d086150848f7a2b5273c15ead1ee9b
unknown <antoine.nowakowski.61@gmail.com>
2023-10-30 19:44:42 +0100

    first commit première fonctions

---
+++ TesteCreationProjetCloudPhoto/Fonctions/Connexion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteCreationProjetCloudPhoto.Fonctions
+{
+    internal class Connexion
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Demande à l'utilisateur de se connecter
+        /// -> email et password
+        /// </summary>
+        /// <param name="email">Email</param>
+        /// <param name="pswrd">Mot de passe</param>
+        public Connexion(string email, string pswrd) 
+        {
+            this.Email = email;  
+            this.Password = pswrd;
+        }
+    }
+}
---
+++ TesteCreationProjetCloudPhoto/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Création de classes et teste du projet cloud photo
+ * app console, et demandes fictives
+ * 
+ * Demander compte ou pas compte
+ * création compte ou connexion
+ * demander importer photos ou supprimer photo
+ */
+using TesteCreationProjetCloudPhoto.Fonctions;
+
+
+#region Fonctions
+
+
+string DemandeCompteCree()
+{
+    bool EntreeUtilisateurNonVide = true;
+    string EntreeUtilisateur;
+
+
+    // demande compte
+    do
+    {
+        Console.WriteLine("Avez-vous un compte ?");
+        EntreeUtilisateur = Console.ReadLine();
+
+        EntreeUtilisateurNonVide = string.IsNullOrEmpty(EntreeUtilisateur);
+
+    } while (EntreeUtilisateurNonVide);
+
+    if (EntreeUtilisateur == "oui".ToLower())
+    {
+        Console.WriteLine("Vous avez un compte");
+        Console.WriteLine("Veillez vous connectez");
+        SeConnecter();
+    }
+    else
+    {
+        if (EntreeUtilisateur == "non".ToLower())
+        {
+            Console.WriteLine("Vous n'avez pas de compte");
+            Console.WriteLine("Veillez vous en créer un");
+        }
+        DemandeCompteCree();
+    }
+
+    return EntreeUtilisateur;
+}
+
+void SeConnecter()
+{
+    Console.WriteLine("Renseignez votre email : ");
+    string email = Console.ReadLine(); 
+    Console.WriteLine("Renseignez votre mot de passe : ");
+    string motDePasse = Console.ReadLine();
+
+    Connexion NouvelleConnexion = new Connexion(email, motDePasse);
+    Console.WriteLine($"Votre email est : {NouvelleConnexion.Email} \nEt votre mot de passe est : {NouvelleConnexion.Password}");
+}
+
+void CreeCompte()
+{
+
+}
+#endregion
+
+DemandeCompteCree();
